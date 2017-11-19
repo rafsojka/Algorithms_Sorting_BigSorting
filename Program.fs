61dcb0e4bcd9e502cb775f9565938a2c1bdcb62e
rafsojka <raf.sojka@gmail.com>
2017-11-19 21:21:02 +0000

    my solution

---
+++ Program.fs
@@ -0,0 +1,26 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open System
+
+[<EntryPoint>]
+let main argv = 
+    let n = Console.ReadLine() |> Int32.Parse
+    let arr = Array.create n ""
+    for i in 0..n-1 do
+        arr.[i] <- Console.ReadLine()
+
+    let sorted_arr = 
+        arr 
+        // 1. groupBy lengths
+        |> Seq.groupBy (fun s -> s.Length) 
+        // 2. sortBy lengths
+        |> Seq.sortBy (fun (k,_) -> k)
+        //  3. within the same lengths sort strings alphabetically
+        |> Seq.map (fun (_,s) -> Seq.sort s) 
+        // 4. flatten into single array
+        |> Seq.concat
+
+    sorted_arr |> Seq.iter (fun i -> printfn "%s" i)
+
+    0 // return an integer exit code
